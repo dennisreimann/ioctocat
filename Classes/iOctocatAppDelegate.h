b98189b83c7bfeb03cba90f5876e0b951aad605e
Dennis Blöte <mail@dennisbloete.de>
2009-03-28 13:42:47 +0100

    Added check for network reachability

--- Classes/iOctocatAppDelegate.h
+++ Classes/iOctocatAppDelegate.h
@@ -2,9 +2,13 @@
 
 
 @interface iOctocatAppDelegate : NSObject <UIApplicationDelegate> {
+  @private
     IBOutlet UIWindow *window;
     IBOutlet UINavigationController *navigationController;
+	BOOL isDataSourceAvailable;
 }
+
+@property (nonatomic, readonly) BOOL isDataSourceAvailable;
 
 @end
 
--- Classes/iOctocatAppDelegate.m
+++ Classes/iOctocatAppDelegate.m
@@ -1,11 +1,49 @@
 #import "iOctocatAppDelegate.h"
 #import "RootViewController.h"
+#import <SystemConfiguration/SystemConfiguration.h>
+
+
+
+@interface iOctocatAppDelegate (PrivateMethods)
+
+- (void)displayNoConnectionView;
+
+@end
 
 
 @implementation iOctocatAppDelegate
 
 - (void)applicationDidFinishLaunching:(UIApplication *)application {
-	[window addSubview:[navigationController view]];
+	if (self.isDataSourceAvailable) {
+		[window addSubview:[navigationController view]];
+    } else {
+		[self displayNoConnectionView];
+	}
+}
+
+- (BOOL)isDataSourceAvailable {
+    static BOOL checkNetwork = YES;
+    if (checkNetwork) {
+        checkNetwork = NO;
+		const char *hostName = "github.com";
+        Boolean success;
+        SCNetworkReachabilityRef reachability = SCNetworkReachabilityCreateWithName(NULL, hostName);
+        SCNetworkReachabilityFlags flags;
+        success = SCNetworkReachabilityGetFlags(reachability, &flags);
+        isDataSourceAvailable = success && (flags & (kSCNetworkFlagsReachable)) && !(flags & kSCNetworkFlagsConnectionRequired);
+        CFRelease(reachability);
+    }
+    return isDataSourceAvailable;
+}
+
+- (void)displayNoConnectionView {
+	UIImage *noConnectionImage = [UIImage imageNamed:@"NoConnection.png"];
+	UIImageView *noConnectionView = [[UIImageView alloc] initWithImage:noConnectionImage];
+	CGRect noConnectionViewFrame = noConnectionView.frame;
+	noConnectionViewFrame.origin = CGPointMake(0.0f, [UIApplication sharedApplication].statusBarFrame.size.height);
+	noConnectionView.frame = noConnectionViewFrame;
+	[window addSubview:noConnectionView];
+	[noConnectionView release];
 }
 
 #pragma mark -

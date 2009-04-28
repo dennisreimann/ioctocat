8025f1c8ca5090c8424499fae4c0998325815d59
dbloete <mail@dennisbloete.de>
2009-04-28 21:20:51 +0200

    Added buttons and URLs for following and watching

--- Classes/AppConstants.h
+++ Classes/AppConstants.h
@@ -18,6 +18,10 @@
 #define kUserReposFormat @"https://github.com/api/v2/xml/repos/show/%@?token=%@"
 #define kRepoXMLFormat @"https://github.com/api/v2/xml/repos/show/%@/%@"
 #define kRepoCommitsXMLFormat @"https://github.com/api/v2/xml/commits/list/%@/%@/%@"
+#define kRepoWatchFormat @"https://github.com/api/v2/xml/repos/watch/%@/%@?token=%@"
+#define kRepoUnwatchFormat @"https://github.com/api/v2/xml/repos/watch/%@/%@?token=%@"
+#define kUserFollowFormat @"https://github.com/api/v2/xml/user/follow/%@?token=%@"
+#define kUserUnfollowFormat @"https://github.com/api/v2/xml/user/unfollow/%@?token=%@"
 
 // KVO
 #define kFeedLoadingKeyPath @"isLoading"

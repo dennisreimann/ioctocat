d27630a78739aba750cd5659b3bce528873ae525
Jake Wharton <JakeWharton@GMail.com>
2009-05-01 11:06:18 -0400

    Lowercase email before md5 so <SteveJobs@apple.com> and <stevejobs@apple.com> get the same hash and gravatar.

--- Classes/GravatarLoader.m
+++ Classes/GravatarLoader.m
@@ -39,7 +39,9 @@ NSString *md5(NSString *str) {
 
 - (void)requestWithArgs:(NSArray *)theArgs {
 	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-	NSString *email = [theArgs objectAtIndex:0];
+	#Lowercase the email since SteveJobs@apple.com and stevejobs@apple.com are
+	# the same person but gravatar only recognizes the md5 of the latter
+	NSString *email = [[theArgs objectAtIndex:0] lowercaseString];
 	NSInteger size = [[theArgs objectAtIndex:1] integerValue];
 	NSString *url = [NSString stringWithFormat:@"http://www.gravatar.com/avatar/%@?s=%d", md5(email), size];
 	NSURL *gravatarURL = [NSURL URLWithString:url];

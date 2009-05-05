479004076dd903a5ae5f2d3b5e0a3e1a176b72f7
dbloete <mail@dennisbloete.de>
2009-05-05 17:43:23 +0200

    Removed pretty date logging

--- Classes/NSDate+Nibware.m
+++ Classes/NSDate+Nibware.m
@@ -10,33 +10,11 @@
 
 
 @implementation NSDate (Nibware)
-/*
- function prettyDate(time){
- var date = new Date((time || "").replace(/-/g,"/").replace(/[TZ]/g," ")),
- diff = (((new Date()).getTime() - date.getTime()) / 1000),
- day_diff = Math.floor(diff / 86400);
- 
- if ( isNaN(day_diff) || day_diff < 0 || day_diff >= 31 )
- return;
- 
- return day_diff == 0 && (
- diff < 60 && "just now" ||
- diff < 120 && "1 minute ago" ||
- diff < 3600 && Math.floor( diff / 60 ) + " minutes ago" ||
- diff < 7200 && "1 hour ago" ||
- diff < 86400 && Math.floor( diff / 3600 ) + " hours ago") ||
- day_diff == 1 && "Yesterday" ||
- day_diff < 7 && day_diff + " days ago" ||
- day_diff < 31 && Math.ceil( day_diff / 7 ) + " weeks ago";
- }
- 
- */
 
 - (NSString*) prettyDateWithReference:(NSDate*)reference
 {
     float diff = [reference timeIntervalSinceDate:self];
     float day_diff = floor(diff / 86400);
-    NSLog(@"PrettyDate: %f seconds diff, day_diff=%f", diff, day_diff);
 
     if (day_diff == 0) {
         if (diff < 60) return @"just now";

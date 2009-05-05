892ca4d7135624c6195bc9a613e3fb8a39e06077
dbloete <mail@dennisbloete.de>
2009-05-05 19:39:08 +0200

    Refactored private repository feed url

--- Classes/GHRepository.m
+++ Classes/GHRepository.m
@@ -28,14 +28,7 @@
 - (void)setOwner:(NSString *)theOwner andName:(NSString *)theName {
 	self.owner = theOwner;
 	self.name = theName;
-        
-    // dont like this from a cleanlyness perspective but I wanted to get it working before we refactor
-	NSString *commitFeedURLString ;
-    if ( self.isPrivate ) {
-       commitFeedURLString = [NSString stringWithFormat:kPrivateRepoFeedFormat, owner, name];
-    } else {
-       commitFeedURLString = [NSString stringWithFormat:kRepoFeedFormat, owner, name];
-    }
+	NSString *commitFeedURLString = [NSString stringWithFormat:kRepoFeedFormat, owner, name];
 	NSURL *commitFeedURL = [NSURL URLWithString:commitFeedURLString];
 	GHFeed *commitFeed = [[GHFeed alloc] initWithURL:commitFeedURL];
 	self.recentCommits = commitFeed;
@@ -54,7 +47,6 @@
 -(int)compareByName:(GHRepository*)repo {
     return [[self name] localizedCaseInsensitiveCompare:[repo name]];
 }
-
 
 #pragma mark -
 #pragma mark Repository loading

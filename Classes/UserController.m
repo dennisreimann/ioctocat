bb80df79258b779e62d76ff622ae98a6d5c381ef
dbloete <mail@dennisbloete.de>
2009-05-06 18:05:24 +0200

    Cleanups

--- Classes/UserController.m
+++ Classes/UserController.m
@@ -168,8 +168,7 @@
 		[[UIApplication sharedApplication] openURL:mailURL];
 		[mailURL release];
 	} else if (section == 1 && row == 0 ) {
-        NSLog(@"user is %@", user);
-        FollowingController *followingController = [[FollowingController alloc] initWithUser:user];
+        FollowingController *followingController = [(FollowingController *)[FollowingController alloc] initWithUser:user];
 		[self.navigationController pushViewController:followingController animated:YES];
 		[followingController release];            
 	} else if (section == 2) {

f29e7f55c6e57df9d2fcefa5e70f94767931ebf5
dbloete <mail@dennisbloete.de>
2010-07-31 07:39:06 +0200

    Revert "Changed auth for token via HTTP Basic Auth" because I am stupid
    This reverts commit 3355010be5a20f126e3a2e1691145e321bf8dea9.

--- Classes/GHResource.m
+++ Classes/GHResource.m
@@ -47,10 +47,8 @@
 	NSString *token = [defaults stringForKey:kTokenDefaultsKey];
 	
 	ASIFormDataRequest *request = [ASIFormDataRequest requestWithURL:url];
-	// Authentication with token via HTTP Basic Auth, see:
-	// http://support.github.com/discussions/api/57-reposshowlogin-is-missing-private-repositories
-	NSString *loginToken = [NSString stringWithFormat:@"%@/token:%@", login, token];
-	[request setUsername:loginToken];
+	[request setUsername:login];
+	[request setPassword:token];
     
 	return request;
 }

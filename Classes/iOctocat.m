3d1e2a38ad99729219a20b8828389cdc51cdb426
Dennis Reimann <mail@dennisreimann.de>
2012-09-14 11:14:07 +0200

    Support iPhone 5 display size

--- Classes/iOctocat.m
+++ Classes/iOctocat.m
@@ -48,6 +48,7 @@ SYNTHESIZE_SINGLETON_FOR_CLASS(iOctocat);
 	
 	// Go
 	self.users = [NSMutableDictionary dictionary];
+	window.frame = [UIScreen mainScreen].bounds;
 	[window setRootViewController:navController];
 	[window makeKeyAndVisible];
 }

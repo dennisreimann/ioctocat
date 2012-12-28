86ca8251446db1ff681b4df1f5d84dc9df8250a2
Dennis Reimann <mail@dennisreimann.de>
2012-12-28 13:57:07 +0100

    Prevent gists from crashing after loading

--- Classes/GHGists.m
+++ Classes/GHGists.m
@@ -17,10 +17,14 @@
 
 - (void)setValues:(id)values {
 	self.items = [NSMutableArray array];
-	for (NSDictionary *dict in values) {
-		GHGist *resource = [[GHGist alloc] initWithId:[dict safeStringForKey:@"id"]];
-		[resource setValues:dict];
-		[self addObject:resource];
+	for (id dict in values) {
+		if ([dict isKindOfClass:NSDictionary.class]) {
+			GHGist *resource = [[GHGist alloc] initWithId:[dict safeStringForKey:@"id"]];
+			[resource setValues:dict];
+			[self addObject:resource];
+		} else {
+			DJLog(@"Could not add gist: Expected a dictionary but got %@", dict);
+		}
 	}
 }
 

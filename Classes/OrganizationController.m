b74987ab985e3bda910f173017767bc177c42fe2
Dennis Reimann <mail@dennisreimann.de>
2013-01-21 21:38:38 +0100

    Refactoring: OrganizationController
    Use blocks instead of KVO

--- Classes/OrganizationController.m
+++ Classes/OrganizationController.m
@@ -47,22 +47,44 @@
 	self = [super initWithNibName:@"Organization" bundle:nil];
 	if (self) {
 		self.organization = organization;
+		[self.organization addObserver:self forKeyPath:kGravatarKeyPath options:NSKeyValueObservingOptionNew context:nil];
 	}
 	return self;
 }
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
-	[self.organization addObserver:self forKeyPath:kResourceLoadingStatusKeyPath options:NSKeyValueObservingOptionNew context:nil];
-	[self.organization addObserver:self forKeyPath:kGravatarKeyPath options:NSKeyValueObservingOptionNew context:nil];
-	[self.organization.repositories addObserver:self forKeyPath:kResourceLoadingStatusKeyPath options:NSKeyValueObservingOptionNew context:nil];
-	[self.organization.publicMembers addObserver:self forKeyPath:kResourceLoadingStatusKeyPath options:NSKeyValueObservingOptionNew context:nil];
-	(self.organization.isLoaded) ? [self displayOrganization] : [self.organization loadData];
-	if (!self.organization.repositories.isLoaded) [self.organization.repositories loadData];
-	if (!self.organization.publicMembers.isLoaded) [self.organization.publicMembers loadData];
 	self.navigationItem.title = self.organization.login;
 	self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAction target:self action:@selector(showActions:)];
-	// Background
+	[self displayOrganization];
+	// load resources
+	if (!self.organization.isLoaded) {
+		[self.organization loadWithParams:nil success:^(GHResource *instance, id data) {
+			[self displayOrganization];
+			[self.tableView reloadData];
+		} failure:^(GHResource *instance, NSError *error) {
+			[iOctocat reportLoadingError:@"Could not load the organization"];
+		}];
+	}
+	if (!self.organization.repositories.isLoaded) {
+		[self.organization.repositories loadWithParams:nil success:^(GHResource *instance, id data) {
+			if (!self.organization.isLoaded) return;
+			NSIndexSet *sections = [NSIndexSet indexSetWithIndex:2];
+			[self.tableView reloadSections:sections withRowAnimation:UITableViewRowAnimationAutomatic];
+		} failure:^(GHResource *instance, NSError *error) {
+			[iOctocat reportLoadingError:@"Could not load the repositories"];
+		}];
+	}
+	if (!self.organization.publicMembers.isLoaded) {
+		[self.organization.publicMembers loadWithParams:nil success:^(GHResource *instance, id data) {
+			if (!self.organization.isLoaded) return;
+			NSIndexSet *sections = [NSIndexSet indexSetWithIndex:3];
+			[self.tableView reloadSections:sections withRowAnimation:UITableViewRowAnimationAutomatic];
+		} failure:^(GHResource *instance, NSError *error) {
+			[iOctocat reportLoadingError:@"Could not load the members"];
+		}];
+	}
+	// header
 	UIColor *background = [UIColor colorWithPatternImage:[UIImage imageNamed:@"HeadBackground80.png"]];
 	self.tableHeaderView.backgroundColor = background;
 	self.tableView.tableHeaderView = self.tableHeaderView;
@@ -71,10 +93,7 @@
 }
 
 - (void)dealloc {
-	[self.organization removeObserver:self forKeyPath:kResourceLoadingStatusKeyPath];
 	[self.organization removeObserver:self forKeyPath:kGravatarKeyPath];
-	[self.organization.repositories removeObserver:self forKeyPath:kResourceLoadingStatusKeyPath];
-	[self.organization.publicMembers removeObserver:self forKeyPath:kResourceLoadingStatusKeyPath];
 }
 
 #pragma mark Actions
@@ -92,8 +111,8 @@
 }
 
 - (void)displayOrganization {
-	self.nameLabel.text = (!self.organization.name || [self.organization.name isEmpty]) ? self.organization.login : self.organization.name;
-	self.companyLabel.text = (!self.organization.company || [self.organization.company isEmpty]) ? [NSString stringWithFormat:@"%d followers", self.organization.followersCount] : self.organization.company;
+	self.nameLabel.text = (!self.organization.name || self.organization.name.isEmpty) ? self.organization.login : self.organization.name;
+	self.companyLabel.text = (!self.organization.company || self.organization.company.isEmpty) ? [NSString stringWithFormat:@"%d followers", self.organization.followersCount] : self.organization.company;
 	if (self.organization.gravatar) self.gravatarView.image = self.organization.gravatar;
 	[self.locationCell setContentText:self.organization.location];
 	[self.blogCell setContentText:[self.organization.blogURL host]];
@@ -103,49 +122,29 @@
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
 	if ([keyPath isEqualToString:kGravatarKeyPath]) {
 		self.gravatarView.image = self.organization.gravatar;
-	} else if (object == self.organization && [keyPath isEqualToString:kResourceLoadingStatusKeyPath]) {
-		if (self.organization.isLoaded) {
-			[self displayOrganization];
-			[self.tableView reloadData];
-		} else if (self.organization.error) {
-			[iOctocat reportLoadingError:@"Could not load the organization"];
-		}
-	} else if (object == self.organization.repositories && [keyPath isEqualToString:kResourceLoadingStatusKeyPath]) {
-		if (self.organization.repositories.isLoaded) {
-			[self.tableView reloadData];
-		} else if (self.organization.repositories.error) {
-			[iOctocat reportLoadingError:@"Could not load the repositories"];
-		}
-	} else if (object == self.organization.publicMembers && [keyPath isEqualToString:kResourceLoadingStatusKeyPath]) {
-		if (self.organization.publicMembers.isLoaded) {
-			[self.tableView reloadData];
-		} else if (self.organization.publicMembers.error) {
-			[iOctocat reportLoadingError:@"Could not load the members"];
-		}
 	}
 }
 
 #pragma mark TableView
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-	if (!self.organization.isLoaded) return 1;
-	return 4;
+	return self.organization.isLoaded ? 4 : 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
 	if (!self.organization.isLoaded) return 1;
 	if (section == 0) return 3;
 	if (section == 1) return 1;
-	if (section == 2 && (!self.organization.publicMembers.isLoaded || self.organization.publicMembers.isEmpty)) return 1;
-	if (section == 2) return self.organization.publicMembers.count;
-	if (section == 3 && (!self.organization.repositories.isLoaded || self.organization.repositories.isEmpty)) return 1;
-	if (section == 3) return self.organization.repositories.count;
+	if (section == 2 && (!self.organization.repositories.isLoaded || self.organization.repositories.isEmpty)) return 1;
+	if (section == 2) return self.organization.repositories.count;
+	if (section == 3 && (!self.organization.publicMembers.isLoaded || self.organization.publicMembers.isEmpty)) return 1;
+	if (section == 3) return self.organization.publicMembers.count;
 	return 1;
 }
 
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
-	if (section == 2) return @"Members";
-	if (section == 3) return @"Repositories";
+	if (section == 2) return @"Repositories";
+	if (section == 3) return @"Members";
 	return @"";
 }
 
@@ -167,23 +166,23 @@
 		return cell;
 	}
 	if (section == 1) return self.recentActivityCell;
-	if (section == 2 && !self.organization.publicMembers.isLoaded) return self.loadingMembersCell;
-	if (section == 2 && self.organization.publicMembers.isEmpty) return self.noPublicMembersCell;
+	if (section == 2 && !self.organization.repositories.isLoaded) return self.loadingReposCell;
+	if (section == 2 && self.organization.repositories.isEmpty) return self.noPublicReposCell;
 	if (section == 2) {
+		RepositoryCell *cell = (RepositoryCell *)[tableView dequeueReusableCellWithIdentifier:kRepositoryCellIdentifier];
+		if (cell == nil) cell = [RepositoryCell cell];
+		cell.repository = (self.organization.repositories)[indexPath.row];
+		[cell hideOwner];
+		return cell;
+	}
+	if (section == 3 && !self.organization.publicMembers.isLoaded) return self.loadingMembersCell;
+	if (section == 3 && self.organization.publicMembers.isEmpty) return self.noPublicMembersCell;
+	if (section == 3) {
 		UserObjectCell *cell = (UserObjectCell *)[tableView dequeueReusableCellWithIdentifier:kUserObjectCellIdentifier];
 		if (cell == nil) {
 			cell = [UserObjectCell cell];
 		}
 		cell.userObject = (self.organization.publicMembers)[indexPath.row];
-		return cell;
-	}
-	if (section == 3 && !self.organization.repositories.isLoaded) return self.loadingReposCell;
-	if (section == 3 && self.organization.repositories.isEmpty) return self.noPublicReposCell;
-	if (section == 3) {
-		RepositoryCell *cell = (RepositoryCell *)[tableView dequeueReusableCellWithIdentifier:kRepositoryCellIdentifier];
-		if (cell == nil) cell = [RepositoryCell cell];
-		cell.repository = (self.organization.repositories)[indexPath.row];
-		[cell hideOwner];
 		return cell;
 	}
 	return nil;
@@ -205,11 +204,11 @@
 		viewController = [[EventsController alloc] initWithEvents:self.organization.events];
 		viewController.title = @"Recent Activity";
 	} else if (section == 2) {
-		GHUser *selectedUser = (self.organization.publicMembers)[indexPath.row];
-		viewController = [[UserController alloc] initWithUser:(GHUser *)selectedUser];
-	} else if (section == 3) {
-		GHRepository *repo = (self.organization.repositories)[indexPath.row];
+		GHRepository *repo = self.organization.repositories[indexPath.row];
 		viewController = [[RepositoryController alloc] initWithRepository:repo];
+	} else if (section == 3) {
+		GHUser *selectedUser = self.organization.publicMembers[indexPath.row];
+		viewController = [[UserController alloc] initWithUser:(GHUser *)selectedUser];
 	}
 	// Maybe push a controller
 	if (viewController) {
@@ -220,7 +219,7 @@
 
 #pragma mark MessageComposer Delegate
 
--(void) mailComposeController:(MFMailComposeViewController*)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError*)error {
+- (void)mailComposeController:(MFMailComposeViewController*)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError*)error {
 	[self dismissModalViewControllerAnimated:YES];
 }
 

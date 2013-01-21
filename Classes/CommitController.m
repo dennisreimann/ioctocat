23575116a00cf5147b83bf2482c8eb5dbad1819b
Dennis Reimann <mail@dennisreimann.de>
2013-01-21 21:59:21 +0100

    Refactoring: CommitController
    Use block instead of KVO

--- Classes/CommitController.m
+++ Classes/CommitController.m
@@ -36,36 +36,37 @@
 @property(nonatomic,strong)IBOutlet UITableViewCell *loadingCell;
 @property(nonatomic,strong)IBOutlet UITableViewCell *loadingCommentsCell;
 @property(nonatomic,strong)IBOutlet UITableViewCell *noCommentsCell;
-
-- (void)displayCommit;
-- (void)displayComments;
-- (IBAction)showActions:(id)sender;
-- (IBAction)addComment:(id)sender;
 @end
 
 
 @implementation CommitController
 
-NSString *const CommitLoadingKeyPath = @"loadingStatus";
-NSString *const CommitCommentsLoadingKeyPath = @"comments.loadingStatus";
 NSString *const CommitAuthorGravatarKeyPath = @"author.gravatar";
 
 - (id)initWithCommit:(GHCommit *)commit {
 	self = [super initWithNibName:@"Commit" bundle:nil];
 	if (self) {
 		self.commit = commit;
+		[self.commit addObserver:self forKeyPath:CommitAuthorGravatarKeyPath options:NSKeyValueObservingOptionNew context:nil];
 	}
 	return self;
 }
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
-	[self.commit addObserver:self forKeyPath:CommitLoadingKeyPath options:NSKeyValueObservingOptionNew context:nil];
-	[self.commit addObserver:self forKeyPath:CommitCommentsLoadingKeyPath options:NSKeyValueObservingOptionNew context:nil];
-	[self.commit addObserver:self forKeyPath:CommitAuthorGravatarKeyPath options:NSKeyValueObservingOptionNew context:nil];
 	self.title = [self.commit.commitID substringToIndex:8];
 	self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAction target:self action:@selector(showActions:)];
-	// Background
+	[self displayCommit];
+	// load resources
+	if (!self.commit.isLoaded) {
+		[self.commit loadWithParams:nil success:^(GHResource *instance, id data) {
+			[self displayCommit];
+			[self.tableView reloadData];
+		} failure:^(GHResource *instance, NSError *error) {
+			[iOctocat reportLoadingError:@"Could not load the commit"];
+		}];
+	}
+	// header
 	UIColor *background = [UIColor colorWithPatternImage:[UIImage imageNamed:@"HeadBackground90.png"]];
 	self.tableHeaderView.backgroundColor = background;
 	self.tableView.tableHeaderView = self.tableHeaderView;
@@ -73,16 +74,23 @@ NSString *const CommitAuthorGravatarKeyPath = @"author.gravatar";
 	self.gravatarView.layer.masksToBounds = YES;
 }
 
-- (void)viewWillAppear:(BOOL)animated {
-	[super viewWillAppear:animated];
-	(self.commit.isLoaded) ? [self displayCommit] : [self.commit loadData];
-	(self.commit.comments.isLoaded) ? [self displayComments] : [self.commit.comments loadData];
+// load comments in viewDidAppear, so that comments
+// get reloaded after a new comment has been posted
+- (void)viewDidAppear:(BOOL)animated {
+	[super viewDidAppear:animated];
+	if (!self.commit.comments.isLoaded) {
+		[self.commit.comments loadWithParams:nil success:^(GHResource *instance, id data) {
+			if (!self.commit.isLoaded) return;
+			NSIndexSet *sections = [NSIndexSet indexSetWithIndex:2];
+			[self.tableView reloadSections:sections withRowAnimation:UITableViewRowAnimationAutomatic];
+		} failure:^(GHResource *instance, NSError *error) {
+			[iOctocat reportLoadingError:@"Could not load the comments"];
+		}];
+	}
 }
 
 - (void)dealloc {
 	[self.commit removeObserver:self forKeyPath:CommitAuthorGravatarKeyPath];
-	[self.commit removeObserver:self forKeyPath:CommitCommentsLoadingKeyPath];
-	[self.commit removeObserver:self forKeyPath:CommitLoadingKeyPath];
 }
 
 - (GHUser *)currentUser {
@@ -94,26 +102,21 @@ NSString *const CommitAuthorGravatarKeyPath = @"author.gravatar";
 - (void)displayCommit {
 	self.titleLabel.text = self.commit.message;
 	self.dateLabel.text = [self.commit.committedDate prettyDate];
-	if (self.commit.author.gravatar) self.gravatarView.image = self.commit.author.gravatar;
+    if (self.commit.author.gravatar) {
+		self.gravatarView.image = self.commit.author.gravatar;
+	} else if (!self.commit.author.gravatarURL) {
+		[self.commit.author loadData];
+	}
 	[self.repoCell setContentText:self.commit.repository.repoId];
 	[self.authorCell setContentText:self.commit.author.login];
 	[self.committerCell setContentText:self.commit.committer.login];
 	[self.addedCell setFiles:self.commit.added andDescription:@"added"];
 	[self.removedCell setFiles:self.commit.removed andDescription:@"removed"];
 	[self.modifiedCell setFiles:self.commit.modified andDescription:@"modified"];
-	[self.tableView reloadData];
-}
-
-- (void)displayComments {
-	[self.tableView reloadData];
 }
 
 - (IBAction)showActions:(id)sender {
-	UIActionSheet *actionSheet = [[UIActionSheet alloc] initWithTitle:@"Actions"
-															 delegate:self
-													cancelButtonTitle:@"Cancel"
-											   destructiveButtonTitle:nil
-													otherButtonTitles:@"Add comment", nil];
+	UIActionSheet *actionSheet = [[UIActionSheet alloc] initWithTitle:@"Actions" delegate:self cancelButtonTitle:@"Cancel" destructiveButtonTitle:nil otherButtonTitles:@"Add comment", nil];
 	[actionSheet showInView:self.view];
 }
 
@@ -134,27 +137,13 @@ NSString *const CommitAuthorGravatarKeyPath = @"author.gravatar";
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
 	if ([keyPath isEqualToString:CommitAuthorGravatarKeyPath]) {
 		self.gravatarView.image = self.commit.author.gravatar;
-	} else if ([keyPath isEqualToString:CommitLoadingKeyPath]) {
-		if (self.commit.isLoaded) {
-			[self displayCommit];
-		} else if (self.commit.error) {
-			[iOctocat reportLoadingError:@"Could not load the commit"];
-		}
-	} else if ([keyPath isEqualToString:CommitCommentsLoadingKeyPath]) {
-		if (self.commit.comments.isLoading && self.commit.isLoaded) {
-			[self.tableView reloadSections:[NSIndexSet indexSetWithIndex:2] withRowAnimation:UITableViewRowAnimationNone];
-		} if (self.commit.comments.isLoaded) {
-			[self displayComments];
-		} else if (self.commit.comments.error) {
-			[iOctocat reportLoadingError:@"Could not load the commit comments"];
-		}
 	}
 }
 
 #pragma mark TableView
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-	return (self.commit.isLoaded) ? 3 : 1;
+	return self.commit.isLoaded ? 3 : 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {

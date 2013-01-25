9062e2d6a94ddfc93224069936860ea3a7bd7316
Dennis Reimann <mail@dennisreimann.de>
2013-01-25 11:27:10 +0100

    Fixed pretty date helper. Closes #224

--- Classes/Extensions/NSDate+Nibware.m
+++ Classes/Extensions/NSDate+Nibware.m
@@ -14,22 +14,24 @@
 - (NSString *)prettyDateWithReference:(NSDate *)reference {
 	float diff = [reference timeIntervalSinceDate:self];
 	float day_diff = floor(diff / 86400);
+	int days   = (int)day_diff;
+	int weeks  = (int)ceil(day_diff / 7);
+	int months = (int)ceil(day_diff / 30);
+	int years  = (int)ceil(day_diff / 365);
 	if (day_diff <= 0) {
 		if (diff < 60) return @"just now";
 		if (diff < 120) return @"1 minute ago";
 		if (diff < 3600) return [NSString stringWithFormat:@"%d minutes ago", (int)floor( diff / 60 )];
 		if (diff < 7200) return @"1 hour ago";
 		if (diff < 86400) return [NSString stringWithFormat:@"%d hours ago", (int)floor( diff / 3600 )];
-	} else if (day_diff == 1) {
-		return [NSString stringWithFormat:@"%d day ago", (int)day_diff];
-	} else if (day_diff < 7) {
-		return [NSString stringWithFormat:@"%d days ago", (int)day_diff];
-	} else if (day_diff < 31) {
-		return [NSString stringWithFormat:@"%d weeks ago", (int)ceil( day_diff / 7 )];
-	} else if (day_diff < 365) {
-		return [NSString stringWithFormat:@"%d months ago", (int)ceil( day_diff / 30 )];
+	} else if (days < 7) {
+		return [NSString stringWithFormat:@"%d day%@ ago", days, days == 1 ? @"" : @"s"];
+	} else if (weeks < 4) {
+		return [NSString stringWithFormat:@"%d week%@ ago", weeks, weeks == 1 ? @"" : @"s"];
+	} else if (months < 12) {
+		return [NSString stringWithFormat:@"%d month%@ ago", months, months == 1 ? @"" : @"s"];
 	} else {
-		return [NSString stringWithFormat:@"%d years ago", (int)ceil( day_diff / 365 )];
+		return [NSString stringWithFormat:@"%d year%@ ago", years, years == 1 ? @"" : @"s"];
 	}
 	return [self description];
 }

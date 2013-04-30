1bfbed4c1e71bae369332c080d63b26b38544705
Dennis Reimann <mail@dennisreimann.de>
2013-04-30 23:20:53 +0200

    Update cocoapods

---
+++ Pods/Pods-environment.h
@@ -0,0 +1,68 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// AFNetworking
+#define COCOAPODS_POD_AVAILABLE_AFNetworking
+#define COCOAPODS_VERSION_MAJOR_AFNetworking 1
+#define COCOAPODS_VERSION_MINOR_AFNetworking 2
+#define COCOAPODS_VERSION_PATCH_AFNetworking 0
+
+// AFOAuth2Client
+#define COCOAPODS_POD_AVAILABLE_AFOAuth2Client
+#define COCOAPODS_VERSION_MAJOR_AFOAuth2Client 0
+#define COCOAPODS_VERSION_MINOR_AFOAuth2Client 1
+#define COCOAPODS_VERSION_PATCH_AFOAuth2Client 0
+
+// Base64
+#define COCOAPODS_POD_AVAILABLE_Base64
+#define COCOAPODS_VERSION_MAJOR_Base64 1
+#define COCOAPODS_VERSION_MINOR_Base64 0
+#define COCOAPODS_VERSION_PATCH_Base64 1
+
+// ECSlidingViewController
+#define COCOAPODS_POD_AVAILABLE_ECSlidingViewController
+#define COCOAPODS_VERSION_MAJOR_ECSlidingViewController 0
+#define COCOAPODS_VERSION_MINOR_ECSlidingViewController 9
+#define COCOAPODS_VERSION_PATCH_ECSlidingViewController 3
+
+// Expecta
+#define COCOAPODS_POD_AVAILABLE_Expecta
+#define COCOAPODS_VERSION_MAJOR_Expecta 0
+#define COCOAPODS_VERSION_MINOR_Expecta 2
+#define COCOAPODS_VERSION_PATCH_Expecta 1
+
+// HockeySDK
+#define COCOAPODS_POD_AVAILABLE_HockeySDK
+#define COCOAPODS_VERSION_MAJOR_HockeySDK 3
+#define COCOAPODS_VERSION_MINOR_HockeySDK 0
+#define COCOAPODS_VERSION_PATCH_HockeySDK 0
+
+// SVProgressHUD
+#define COCOAPODS_POD_AVAILABLE_SVProgressHUD
+#define COCOAPODS_VERSION_MAJOR_SVProgressHUD 0
+#define COCOAPODS_VERSION_MINOR_SVProgressHUD 9
+#define COCOAPODS_VERSION_PATCH_SVProgressHUD 0
+
+// SVPullToRefresh
+#define COCOAPODS_POD_AVAILABLE_SVPullToRefresh
+#define COCOAPODS_VERSION_MAJOR_SVPullToRefresh 0
+#define COCOAPODS_VERSION_MINOR_SVPullToRefresh 4
+#define COCOAPODS_VERSION_PATCH_SVPullToRefresh 1
+
+// TPKeyboardAvoiding
+#define COCOAPODS_POD_AVAILABLE_TPKeyboardAvoiding
+#define COCOAPODS_VERSION_MAJOR_TPKeyboardAvoiding 0
+#define COCOAPODS_VERSION_MINOR_TPKeyboardAvoiding 0
+#define COCOAPODS_VERSION_PATCH_TPKeyboardAvoiding 1
+
+// YRDropdownView
+#define COCOAPODS_POD_AVAILABLE_YRDropdownView
+#define COCOAPODS_VERSION_MAJOR_YRDropdownView 0
+#define COCOAPODS_VERSION_MINOR_YRDropdownView 2
+#define COCOAPODS_VERSION_PATCH_YRDropdownView 1
+

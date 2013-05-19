faef9163571c3f09608cffed17f97dc2c8dd47ea
Dennis Reimann <mail@dennisreimann.de>
2013-05-19 08:07:00 +0200

    Markdown parser improvements

--- Classes/Extensions/NSString+GHFMarkdown.m
+++ Classes/Extensions/NSString+GHFMarkdown.m
@@ -6,6 +6,7 @@
 //  http://dennisreimann.de
 //
 
+#import <CoreText/CoreText.h>
 #import "NSString+GHFMarkdown.h"
 
 @implementation NSString (GHFMarkdown)
@@ -17,6 +18,14 @@
 - (NSAttributedString *)attributedStringFromMarkdownWithAttributes:(NSDictionary *)attrs {
     NSMutableAttributedString *output = [[NSMutableAttributedString alloc] initWithString:self attributes:attrs];
     NSMutableString *string = output.mutableString;
+    NSArray *matches = nil;
+    UIFont *font = [attrs valueForKey:(NSString *)kCTFontAttributeName];
+    if (!font) font = [UIFont systemFontOfSize:15.0f];
+    CGFloat fontSize = font.pointSize;
+    CTFontRef fontRef = CTFontCreateWithName((__bridge CFStringRef)font.fontName, fontSize, NULL);
+    CTFontRef boldFontRef = CTFontCreateCopyWithSymbolicTraits(fontRef, fontSize, NULL, kCTFontBoldTrait, kCTFontBoldTrait);
+    CTFontRef italicFontRef = CTFontCreateCopyWithSymbolicTraits(fontRef, fontSize, NULL, kCTFontItalicTrait, kCTFontItalicTrait);
+    NSDictionary *codeAttributes = [NSDictionary dictionaryWithObjects:@[[UIFont fontWithName:@"Courier" size:fontSize], (id)[[UIColor darkGrayColor] CGColor]] forKeys:@[(NSString *)kCTFontAttributeName, (NSString *)kCTForegroundColorAttributeName]];
     // links
     NSEnumerator *links = [[string markdownLinks] reverseObjectEnumerator];
     for (NSDictionary *link in links) {
@@ -24,12 +33,67 @@
         NSString *title = link[@"title"];
         [string replaceCharactersInRange:range withString:title];
 	}
+    // bold
+    NSRegularExpression *boldRegex = [[NSRegularExpression alloc] initWithPattern:@"[*|_]{2}(.+?)[*|_]{2}" options:NSRegularExpressionCaseInsensitive error:NULL];
+    matches = [boldRegex matchesInString:string options:NSMatchingReportCompletion range:NSMakeRange(0, string.length)];
+    for (NSTextCheckingResult *match in matches) {
+        NSRange textRange = [match rangeAtIndex:1];
+        NSString *text = [string substringWithRange:textRange];
+        [string replaceCharactersInRange:match.range withString:text];
+        textRange = NSMakeRange(match.range.location, text.length);
+        NSDictionary *boldAttributes = [NSDictionary dictionaryWithObject:(id)CFBridgingRelease(boldFontRef) forKey:(NSString *)kCTFontAttributeName];
+        [output addAttributes:boldAttributes range:textRange];
+	}
+    // italic
+    NSRegularExpression *italicRegex = [[NSRegularExpression alloc] initWithPattern:@"[*|_]{1}(.+?)[*|_]{1}" options:NSRegularExpressionCaseInsensitive error:NULL];
+    matches = [italicRegex matchesInString:string options:NSMatchingReportCompletion range:NSMakeRange(0, string.length)];
+    for (NSTextCheckingResult *match in matches) {
+        NSRange textRange = [match rangeAtIndex:1];
+        NSString *text = [string substringWithRange:textRange];
+        [string replaceCharactersInRange:match.range withString:text];
+        textRange = NSMakeRange(match.range.location, text.length);
+        NSDictionary *italicAttributes = [NSDictionary dictionaryWithObject:(id)CFBridgingRelease(italicFontRef) forKey:(NSString *)kCTFontAttributeName];
+        [output addAttributes:italicAttributes range:textRange];
+	}
+    // tasks
+    NSRegularExpression *taskRegex = [[NSRegularExpression alloc] initWithPattern:@"(-\\s?\\[([\\sx])\\]){1}\\s(.+)" options:NSRegularExpressionCaseInsensitive error:NULL];
+    matches = [taskRegex matchesInString:string options:NSMatchingReportCompletion range:NSMakeRange(0, string.length)];
+    NSEnumerator *tasks = [matches reverseObjectEnumerator];
+    for (NSTextCheckingResult *match in tasks) {
+        NSRange textRange = [match rangeAtIndex:1];
+        NSRange checkRange = [match rangeAtIndex:2];
+        NSString *check = [string substringWithRange:checkRange];
+        BOOL checked = [check isEqualToString:@"x"];
+        NSString *mark = checked ? @"\U00002611" : @"\U000025FB";
+        [string replaceCharactersInRange:textRange withString:mark];
+	}
+    // code block
+    NSRegularExpression *codeBlockRegex = [[NSRegularExpression alloc] initWithPattern:@"`{3}(.+)`{3}" options:(NSRegularExpressionCaseInsensitive|NSRegularExpressionDotMatchesLineSeparators) error:NULL];
+    matches = [codeBlockRegex matchesInString:string options:NSMatchingReportCompletion range:NSMakeRange(0, string.length)];
+    for (NSTextCheckingResult *match in matches) {
+        NSRange textRange = [match rangeAtIndex:1];
+        NSString *text = [string substringWithRange:textRange];
+        [string replaceCharactersInRange:match.range withString:text];
+        textRange = [string rangeOfString:text];
+        [output addAttributes:codeAttributes range:textRange];
+	}
+    // inline code
+    NSRegularExpression *codeInlineRegex = [[NSRegularExpression alloc] initWithPattern:@"`{1}(.+)`{1}" options:NSRegularExpressionCaseInsensitive error:NULL];
+    matches = [codeInlineRegex matchesInString:string options:NSMatchingReportCompletion range:NSMakeRange(0, string.length)];
+    for (NSTextCheckingResult *match in matches) {
+        NSRange textRange = [match rangeAtIndex:1];
+        NSString *text = [string substringWithRange:textRange];
+        [string replaceCharactersInRange:match.range withString:text];
+        textRange = NSMakeRange(match.range.location, text.length);
+        [output addAttributes:codeAttributes range:textRange];
+	}
     return output;
 }
 
+// also takes care of images
 - (NSArray *)markdownLinks {
     NSString *string = self;
-    NSRegularExpression *regex = [[NSRegularExpression alloc] initWithPattern:@"\\[(.*?)\\]\\((\\S+)(\\s+(\"|\')(.*?)(\"|\'))?\\)" options:NSRegularExpressionCaseInsensitive error:NULL];
+    NSRegularExpression *regex = [[NSRegularExpression alloc] initWithPattern:@"!?\\[(.*?)\\]\\((\\S+)(\\s+(\"|\')(.*?)(\"|\'))?\\)" options:NSRegularExpressionCaseInsensitive error:NULL];
     NSArray *matches = [regex matchesInString:string options:NSMatchingReportCompletion range:NSMakeRange(0, string.length)];
     NSMutableArray *results = [[NSMutableArray alloc] initWithCapacity:matches.count];
     for (NSTextCheckingResult *match in matches) {
--- iOctocatUnitTests/NSString+GHFMarkdownTests.m
+++ iOctocatUnitTests/NSString+GHFMarkdownTests.m
@@ -1,3 +1,4 @@
+#import <CoreText/CoreText.h>
 #import "IOCTestHelper.h"
 #import "NSString+GHFMarkdownTests.h"
 #import "NSString+GHFMarkdown.h"
@@ -5,20 +6,63 @@
 
 @implementation NSString_GHFMarkdownTests
 
+- (void)testAttributedStringFromMarkdownWithBold {
+    NSMutableAttributedString *expected = [[NSMutableAttributedString alloc] initWithString:@"This is bold."];
+    UIFont *font = [UIFont boldSystemFontOfSize:15];
+    CTFontRef fontRef = CTFontCreateWithName((__bridge CFStringRef)font.fontName, font.pointSize, NULL);
+    NSDictionary *attributes = [NSDictionary dictionaryWithObject:(id)CFBridgingRelease(fontRef) forKey:(NSString *)kCTFontAttributeName];
+    [expected addAttributes:attributes range:NSMakeRange(5, 2)];
+	expect([@"This **is** bold." attributedStringFromMarkdown]).to.equal(expected);
+}
+
+- (void)testAttributedStringFromMarkdownWithItalic {
+    NSMutableAttributedString *expected = [[NSMutableAttributedString alloc] initWithString:@"This is italic."];
+    UIFont *font = [UIFont italicSystemFontOfSize:15];
+    CTFontRef fontRef = CTFontCreateWithName((__bridge CFStringRef)font.fontName, font.pointSize, NULL);
+    NSDictionary *attributes = [NSDictionary dictionaryWithObject:(id)CFBridgingRelease(fontRef) forKey:(NSString *)kCTFontAttributeName];
+	[expected addAttributes:attributes range:NSMakeRange(5, 2)];
+	expect([@"This *is* italic." attributedStringFromMarkdown]).to.equal(expected);
+}
+
+- (void)testAttributedStringFromMarkdownWithCodeInline {
+    NSMutableAttributedString *expected = [[NSMutableAttributedString alloc] initWithString:@"This is code."];
+    NSDictionary *attributes = [NSDictionary dictionaryWithObjects:@[[UIFont fontWithName:@"Courier" size:15], (id)[[UIColor darkGrayColor] CGColor]] forKeys:@[(NSString *)kCTFontAttributeName, (NSString *)kCTForegroundColorAttributeName]];
+    [expected addAttributes:attributes range:NSMakeRange(5, 2)];
+	expect([@"This `is` code." attributedStringFromMarkdown]).to.equal(expected);
+}
+
+- (void)testAttributedStringFromMarkdownWithCodeBlock {
+	NSMutableAttributedString *expected = [[NSMutableAttributedString alloc] initWithString:@"This has\n\n ruby\nputs 'Test'\n\n\na code block."];
+    NSDictionary *attributes = [NSDictionary dictionaryWithObjects:@[[UIFont fontWithName:@"Courier" size:15], (id)[[UIColor darkGrayColor] CGColor]] forKeys:@[(NSString *)kCTFontAttributeName, (NSString *)kCTForegroundColorAttributeName]];
+    [expected addAttributes:attributes range:NSMakeRange(10, 18)];
+	expect([@"This has\n\n``` ruby\nputs 'Test'\n```\n\na code block." attributedStringFromMarkdown]).to.equal(expected);
+}
+
+- (void)testAttributedStringFromMarkdownWithTasks {
+    NSAttributedString *expected = [[NSAttributedString alloc] initWithString:@"This has\n\n☑ A task\n☐ List\nwithin."];
+	expect([@"This has\n\n- [x] A task\n- [ ] List\nwithin." attributedStringFromMarkdown]).to.equal(expected);
+}
+
 - (void)testAttributedStringFromMarkdownWithLink {
-	expect([@"This [is a link](http://ioctocat.com)." attributedStringFromMarkdown]).to.equal([[NSAttributedString alloc] initWithString:@"This is a link."]);
+    NSAttributedString *expected = [[NSAttributedString alloc] initWithString:@"This is a link."];
+	expect([@"This [is a link](http://ioctocat.com)." attributedStringFromMarkdown]).to.equal(expected);
+}
+
+- (void)testAttributedStringFromMarkdownWithImage {
+    NSAttributedString *expected = [[NSAttributedString alloc] initWithString:@"This is an image."];
+	expect([@"This ![is an image](http://ioctocat.com/img/iOctocat-GitHub_iOS.png)." attributedStringFromMarkdown]).to.equal(expected);
 }
 
 - (void)testGhfmarkdownLinks {
-    NSString *string = @"This [is a link](http://ioctocat.com).";
+    NSString *string = @"This [is a link](http://ioctocat.com) and this ![an image](http://ioctocat.com/img/iOctocat-GitHub_iOS.png).";
     NSArray *links = [string markdownLinks];
-    
-	expect(links.count).to.equal(1);
-    
     NSDictionary *link = links[0];
-    
+    NSDictionary *image = links[1];
+	expect(links.count).to.equal(2);
 	expect(link[@"title"]).to.equal(@"is a link");
 	expect(link[@"url"]).to.equal([NSURL URLWithString:@"http://ioctocat.com"]);
+	expect(image[@"title"]).to.equal(@"an image");
+	expect(image[@"url"]).to.equal([NSURL URLWithString:@"http://ioctocat.com/img/iOctocat-GitHub_iOS.png"]);
 }
 
 @end

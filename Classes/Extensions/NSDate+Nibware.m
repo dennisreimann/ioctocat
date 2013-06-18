2522454497ef2408ad0b2c32485e88a00e008dbf
Dennis Reimann <mail@dennisreimann.de>
2013-06-18 20:47:51 +0200

    Tweak prettyDate to handle future dates

--- Classes/Extensions/NSDate+Nibware.m
+++ Classes/Extensions/NSDate+Nibware.m
@@ -12,7 +12,12 @@
 @implementation NSDate (Nibware)
 
 - (NSString *)prettyDateWithReference:(NSDate *)reference {
+    NSString *inTime = @"ago";
 	float diff = [reference timeIntervalSinceDate:self];
+    if (diff < 0) {
+        diff = [self timeIntervalSinceDate:reference];
+        inTime = @"from now";
+    }
 	float day_diff = floor(diff / 86400);
 	int days   = (int)day_diff;
 	int weeks  = (int)ceil(day_diff / 7);
@@ -20,24 +25,24 @@
 	int years  = (int)ceil(day_diff / 365);
 	if (day_diff <= 0) {
 		if (diff < 60) return @"just now";
-		if (diff < 120) return @"1 minute ago";
-		if (diff < 3600) return [NSString stringWithFormat:@"%d minutes ago", (int)floor( diff / 60 )];
-		if (diff < 7200) return @"1 hour ago";
-		if (diff < 86400) return [NSString stringWithFormat:@"%d hours ago", (int)floor( diff / 3600 )];
+		if (diff < 120) return [NSString stringWithFormat:@"1 minute %@", inTime];
+		if (diff < 3600) return [NSString stringWithFormat:@"%d minutes %@", (int)floor(diff / 60), inTime];
+		if (diff < 7200) return [NSString stringWithFormat:@"1 hour %@", inTime];
+		if (diff < 86400) return [NSString stringWithFormat:@"%d hours %@", (int)floor(diff / 3600), inTime];
 	} else if (days < 7) {
-		return [NSString stringWithFormat:@"%d day%@ ago", days, days == 1 ? @"" : @"s"];
+		return [NSString stringWithFormat:@"%d day%@ %@", days, days == 1 ? @"" : @"s", inTime];
 	} else if (weeks < 4) {
-		return [NSString stringWithFormat:@"%d week%@ ago", weeks, weeks == 1 ? @"" : @"s"];
+		return [NSString stringWithFormat:@"%d week%@ %@", weeks, weeks == 1 ? @"" : @"s", inTime];
 	} else if (months < 12) {
-		return [NSString stringWithFormat:@"%d month%@ ago", months, months == 1 ? @"" : @"s"];
+		return [NSString stringWithFormat:@"%d month%@ %@", months, months == 1 ? @"" : @"s", inTime];
 	} else {
-		return [NSString stringWithFormat:@"%d year%@ ago", years, years == 1 ? @"" : @"s"];
+		return [NSString stringWithFormat:@"%d year%@ %@", years, years == 1 ? @"" : @"s", inTime];
 	}
-	return [self description];
+	return self.description;
 }
 
 - (NSString *)prettyDate {
-	return [self prettyDateWithReference:[NSDate date]];
+	return [self prettyDateWithReference:NSDate.date];
 }
 
 @end

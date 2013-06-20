0b69a731bd5b8f468ae04aeff0a18259241c5d74
Dennis Reimann <mail@dennisreimann.de>
2013-06-20 13:50:46 +0200

    LabeledCell: Allow to specify empty text

--- Classes/IOCLabeledCell.h
+++ Classes/IOCLabeledCell.h
@@ -1,4 +1,5 @@
 @interface IOCLabeledCell : UITableViewCell
+@property(nonatomic,strong)NSString *emptyText;
 @property(nonatomic,readonly)BOOL hasContent;
 
 - (void)setLabelText:(NSString *)text;
--- Classes/IOCLabeledCell.m
+++ Classes/IOCLabeledCell.m
@@ -15,9 +15,16 @@
 	self.label.text = text;
 }
 
+- (NSString *)emptyText {
+    if (!_emptyText) {
+        _emptyText = NSLocalizedString(@"n/a", @"Labeled Cell: not available (indicates no content)");
+    }
+    return _emptyText;
+}
+
 - (void)setContentText:(NSString *)text {
-	self.hasContent = (![text isKindOfClass:NSNull.class] && text != nil && ![text isEmpty]);
-	self.content.text = self.hasContent ? text : @"n/a";
+	self.hasContent = (![text isKindOfClass:NSNull.class] && text != nil && !text.isEmpty);
+	self.content.text = self.hasContent ? text : self.emptyText;
 	self.content.textColor = self.hasContent ? [UIColor blackColor] : [UIColor grayColor];
 	self.content.highlightedTextColor = [UIColor whiteColor];
 }
